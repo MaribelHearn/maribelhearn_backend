2dc135834298ce137b5c425fd1ab5b6ff7cd0d37
Maribel Hearn <maribelhearn@disroot.org>
2024-04-26 11:44:36 +0200

    Replay support

--- replays/models.py
+++ replays/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+def replay_dir(instance, filename):
+        if instance.category.type == "LNN":
+            return f"replays/lnn/{instance.player}/{instance.category.shot.game.code}{instance.category.code}.rpy"
+        return f"replays/{instance.category.shot.game.code}{instance.category.code}.rpy"
+
+
 class Game(models.Model):
     full_name = models.CharField(max_length=128)
     short_name = models.CharField(max_length=16, unique=True)
+    code = models.CharField(blank=True, max_length=16)
 
     def __str__(self):
         return self.short_name
@@ -32,10 +39,12 @@ class Category(models.Model):
 
     type = models.CharField(choices=CategoryType, max_length=32)
     shot = models.ForeignKey(ShotType, on_delete=models.CASCADE)
+    route = models.CharField(blank=True, max_length=16)
     difficulty = models.CharField(choices=Difficulty.choices, max_length=32)
+    code = models.CharField(blank=True, max_length=16)
 
     def __str__(self):
-        return f"{self.shot} {self.difficulty} {self.type}"
+        return f"{self.shot} {self.route} {self.difficulty} {self.type}"
 
 
 class Replay(models.Model):
@@ -43,7 +52,7 @@ class Replay(models.Model):
     date = models.DateField()
     submitted_date = models.DateField(auto_now=True)
     player = models.CharField(max_length=128)
-    replay = models.URLField(blank=True, max_length=256)
+    replay = models.FileField(blank=True, upload_to=replay_dir)
     video = models.URLField(blank=True, max_length=256)
 
     def __str__(self):
