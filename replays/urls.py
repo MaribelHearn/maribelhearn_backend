0d7afdf1f28a771ed04713e8639612fc676d3a41
Nick <nckgriva208@gmail.com>
2024-05-20 20:41:39 +0700

    basename for urls

--- replays/urls.py
+++ replays/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .views import category, replay, game
 
 router = routers.DefaultRouter()
-router.register("replay", replay.ReplayViewSet, "replay")
-router.register("category", category.CategoryViewSet, "category")
-router.register("game", game.GameViewSet, "game")
+router.register("replay", replay.ReplayViewSet, basename="replay")
+router.register("category", category.CategoryViewSet, basename="category")
+router.register("game", game.GameViewSet, basename="game")
 
 urlpatterns = router.urls
