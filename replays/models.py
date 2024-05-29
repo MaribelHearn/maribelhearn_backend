765c939b1957ccc16ecd7a7a98f769eedb808b5e
Maribel Hearn <maribelhearn@disroot.org>
2024-05-30 00:15:35 +0200

    Allow blank dates again

--- replays/models.py
+++ replays/models.py
@@ -109,7 +109,7 @@ class Replay(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="replays"
     )
-    date = models.DateField(null=True)
+    date = models.DateField(blank=True, null=True)
     submitted_date = models.DateField(auto_now=True)
     player = models.CharField(max_length=128)
     replay = models.FileField(blank=True, upload_to=replay_dir)
