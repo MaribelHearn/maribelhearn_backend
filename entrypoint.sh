2bb74c5d7b770574a07bbb8904640282527749e6
Maribel Hearn <maribelhearn@disroot.org>
2025-07-04 12:00:24 +0200

    Update logfile format

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 python ./manage.py collectstatic --noinput
 python ./manage.py migrate --noinput
 python ./manage.py qcluster &
-python -m gunicorn --access-logfile - --bind 0.0.0.0:6969 touhou_replay_database.wsgi
+python -m gunicorn --access-logfile - --access-logformat '%({X-Real-IP}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' --bind 0.0.0.0:6969 touhou_replay_database.wsgi
