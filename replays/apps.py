e22979b201c566202dcb6fa4ddf531e274a01e14
Nick <nckgriva208@gmail.com>
2026-02-16 12:08:23 +0700

    invalidate cache on category event

--- replays/apps.py
+++ replays/apps.py
@@ -1,4 +1,8 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+from .models import Category
 
 
 class ReplaysConfig(AppConfig):
@@ -7,3 +11,8 @@ class ReplaysConfig(AppConfig):
 
     def ready(self):
         from . import signals
+
+
+@receiver([post_save, post_delete], sender=Category)
+def clear_my_model_cache(sender, **kwargs):
+    cache.delete("categories_cache")
